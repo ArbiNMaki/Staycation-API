ccec52ab11eada803e91c5b8b18d79dc4dfc5f17
unknown <arbi.nmaki919@gmail.com>
2021-03-19 13:00:09 +0700

    Lupa di commit

---
+++ routes/admin.js
@@ -0,0 +1,23 @@
+const router = require('express').Router()
+const adminController = require('../controller/adminController')
+const { upload, uploadMultiple } = require('../middlewares/multer')
+
+router.get('/dashboard', adminController.viewDashboard)
+
+router.get('/category', adminController.viewCategory)
+router.post('/category', adminController.addCategory)
+router.put('/category', adminController.editCategory)
+router.delete('/category/:id', adminController.deleteCategory)
+
+router.get('/bank', adminController.viewBank)
+router.post('/bank', upload, adminController.addBank)
+router.put('/bank', upload, adminController.editBank)
+router.delete('/bank/:id', adminController.deleteBank)
+
+router.get('/item', adminController.viewItem)
+router.post('/item', uploadMultiple, adminController.addItem)
+router.get('/item/show-image/:id', adminController.showImageItem)
+
+router.get('/booking', adminController.viewBooking)
+
+module.exports = router
